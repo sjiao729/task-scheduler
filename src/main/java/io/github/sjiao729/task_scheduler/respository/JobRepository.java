e3f9052195474d7b72dcb5f942d4998d9eef9828
sjiao729 <sjiao@ucsd.edu>
2026-09-05 10:26:14 -0700

    Added findEligibleJobs method to JobRepository

--- src/main/java/io/github/sjiao729/task_scheduler/respository/JobRepository.java
+++ src/main/java/io/github/sjiao729/task_scheduler/respository/JobRepository.java
@@ -19,4 +19,16 @@ public interface JobRepository extends JpaRepository<Job, UUID>
      * Finds pending jobs by FIFO order
      */
     List<Job> findByStatusOrderByCreatedAtAsc(JobStatus status);
+
+    /**
+     * Finds jobs with the specified status which are scheduled for retry 
+     * already, in FIFO order
+     * @param status the status of the jobs to search for
+     * @param now the current time, to determine if a job is already scheduled
+     */
+    @Query( "SELECT j FROM Job j " + "WHERE j.status = :status " + 
+        "AND (j.nextAttemptAt IS NULL OR j.nextAttemptAt <= :now) " + 
+        "ORDER BY j.createdAt ASC" )
+    List<Job> findEligibleJobs( @Param("status") JobStatus status, 
+        @Param("now") Instant now );
 }
